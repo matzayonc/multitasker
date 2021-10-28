c562abef70830f3e555f23cab2069becf8e928d0
deNeedo <jkudrys@protonmail.com>
2021-10-28 21:23:43 +0200

    Quick token fix in main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from os import read
 import discord, commands.connect, commands.disconnect; from helpers.config import prefix, bot
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{prefix}help"))
     print(f"{bot.user} is now online!")
-bot.run("ODk3OTY4NjUzNzQyNTE4MzMy.YWdYeg.qq7uGUAkobaz2J2NciFbLgqdpxA")
+file = open("./TOKEN.txt", "r")
+token = file.read()
+bot.run(f"{token}")
