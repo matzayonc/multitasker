67c14aca5beefdadf2ab5c725d409253698f6168
deNeedo <jkudrys@protonmail.com>
2022-01-11 00:16:58 +0100

    Changed date format in time command and added counter option to check how many times you have been drinking

--- commands/time.py
+++ commands/time.py
@@ -1,8 +1,8 @@
-import configparser
 import discord, configparser
-from datetime import date
+from datetime import datetime
 
 async def f_time(context, parameter):
+    text_channel = discord.utils.get(context.guild.text_channels, name = "bot")
     parser = configparser.ConfigParser()
     parser.read("./config.env")
     times = parser.get('time', 'time_alcohol')
@@ -12,7 +12,7 @@ async def f_time(context, parameter):
         if name in times:
             await context.author.send(f"There already are stats for {context.author}")
         else:
-            times += f" {context.author}.{date.today()}"
+            times += f" {context.author}.{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.{0}"
             parser.set("time", "time_alcohol", times)
             with open("./config.env", "w") as config:
                 parser.write(config)
@@ -23,7 +23,7 @@ async def f_time(context, parameter):
             for m in range(0, len(entries), 1):
                 if entries[m].split(".")[0] == name:
                     time = entries[m].split(".")
-                    time = f"{time[0]}.{date.today()}"
+                    time = f"{time[0]}.{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.{int(time[2]) + 1}"
                     entries[m] = time
                     break
             times = ""
@@ -43,7 +43,18 @@ async def f_time(context, parameter):
                 if entries[m].split(".")[0] == name:
                     time = entries[m].split(".")[1]
                     break
-            await context.author.send(f"You haven't been drinking since: {time} (Y-M-D format)")
+            await text_channel.send(f"{context.author} hasn't been drinking since: {time}")
+        else:
+            await context.author.send(f"No stats for {context.author} (use 'set' option first)")
+    elif parameter == "counter":
+        name = str(context.author)
+        if name in times:
+            entries = times.split(" ")
+            for m in range(0, len(entries), 1):
+                if entries[m].split(".")[0] == name:
+                    time = entries[m].split(".")[2]
+                    break
+            await text_channel.send(f"{context.author} has been drinking {time} times")
         else:
             await context.author.send(f"No stats for {context.author} (use 'set' option first)")
     else:
