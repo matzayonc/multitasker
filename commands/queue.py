c1bcdc832487f9a67f76a821e913436cd5559e4c
Mateusz Zając <matzayonc@gmail.com>
2022-02-12 20:31:06 +0100

    added webm support

--- commands/queue.py
+++ commands/queue.py
@@ -1,10 +1,14 @@
-import discord, os
+import discord
+import os
+
 
 async def f_queue(context):
-    text_channel = discord.utils.get(context.guild.text_channels, name = "bot")
+    text_channel = discord.utils.get(context.guild.text_channels, name="bot")
     temp = os.listdir("./queue/")
     queue = []
     for m in range(0, len(temp), 1):
         if ".mp3" in temp[m]:
+            queue.append(temp[m])
+        if ".webm" in temp[m]:
             queue.append(temp[m])
     await text_channel.send(f"Queue: {queue}")
